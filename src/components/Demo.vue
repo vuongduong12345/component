cedebdd9f5021766cdac23f57da74e06139ad35d
vuongduong12345 <vuongtatduong@gmail.com>
2019-12-27 15:49:00 +0700

    Create Demo.vue

---
+++ src/components/Demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h1>{{ haha }}</h1>
+    <h2>{{ huhu }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "demo",
+  drops: {
+    title: String
+  },
+  data() {
+    return {
+      haha: "xin chao",
+      huhu: "tam biet"
+    };
+  }
+};
+</script>
