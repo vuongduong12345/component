863fc071dd69c5f0d8dc2bb666957499e022035d
vuongduong12345 <vuongtatduong@gmail.com>
2019-12-27 15:49:04 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Demo title="123456"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/Demo'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Demo
   }
 }
 </script>
